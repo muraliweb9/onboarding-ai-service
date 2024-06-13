bd2f625cf0118919cd6e8aacf6a08dd649c942fc
muraliweb9 <muraliweb@hotmail.com>
2024-06-13 20:51:36 +0100

    Initial

---
+++ src/main/java/com/ai/natwest/onboarding/ai/OnboardingAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai.natwest.onboarding.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingAiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/natwest/onboarding/ai/OnboardingAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.natwest.onboarding.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
